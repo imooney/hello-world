9308b55eebe029ec6c12eceddcd505947553426f
imooney <fs3383@wayne.edu>
2016-09-27 12:24:14 -0400

    stuff

--- HelloWorld.cxx
+++ HelloWorld.cxx
@@ -17,6 +17,6 @@ void HelloWorld () {
     }
     gr = new TGraph(n,x,y);
     gr->Draw("AC*");
-    //here's some even eeeeeven even more stuff
+    //here's some even eeeeeven even even more stuff
     return;
 }
